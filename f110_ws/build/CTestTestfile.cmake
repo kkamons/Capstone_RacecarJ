f22e0a3b8d9821080c3c0383a4c005b972528899
Your Name <you@example.com>
2020-01-24 13:34:04 -0600

    updated files and added .gitignore 1/24

---
+++ f110_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/nvidia/Capstone_RacecarJ/f110_ws/src
+# Build directory: /home/nvidia/Capstone_RacecarJ/f110_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(f110-fall2018-skeletons/labs/gap_finding)
+subdirs(f110-fall2018-skeletons/labs/lab_pure_pursuit)
+subdirs(f110-fall2018-skeletons/labs/wall_following)
---
+++ f110_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/nvidia/Capstone_RacecarJ/f110_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/nvidia/ydlidar_ws/devel;/opt/ros/kinetic".split(';') if "/home/nvidia/ydlidar_ws/devel;/opt/ros/kinetic" != "" else []
